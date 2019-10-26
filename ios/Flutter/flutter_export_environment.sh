b50318b00b2141bc46df17791b3fb67b412cf471
susutw <sudosu.tw@gmail.com>
2019-10-26 20:38:40 +0800

    add firebase version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sddivid/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sddivid/Documents/flutter_app/192-flutter-fcm-push-notifications"
+export "FLUTTER_ROOT=/Users/susutw/Documents/FlutterApp/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/susutw/Documents/FlutterAppTest/Flutter-FCM-By-GDG-"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/sddivid/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/susutw/Documents/FlutterApp/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1.0.0"
